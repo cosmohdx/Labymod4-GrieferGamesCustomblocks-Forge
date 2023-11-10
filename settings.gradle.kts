b00d2f483d412daa049cd2e7cd5a4ec8bd5d7dda
Christian Linde <info@christian-linde.de>
2023-11-10 07:34:30 +0100

    new labymod version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 rootProject.name = "griefergames_customblocks_forge"
 
 pluginManagement {
-    val labyGradlePluginVersion = "0.3.25"
+    val labyGradlePluginVersion = "0.3.30"
     plugins {
         id("net.labymod.gradle") version (labyGradlePluginVersion)
     }
